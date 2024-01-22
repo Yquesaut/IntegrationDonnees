b736d2648346b2eecc3700c0973292333247a416
Yquesaut <ixop.ed@gmail.com>
2024-01-22 15:02:52 +0100

    Init

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import org.apache.spark.ml.feature.StringIndexer;
+import org.apache.spark.ml.feature.StringIndexerModel;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
