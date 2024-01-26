5408d767c648453e5ca9fbb0061e2d1dd45896fa
bassaoudm <mohamed.bassaoud@conserto.pro>
2024-01-26 01:14:49 +0100

    first commit

---
+++ src/main/java/fr/integration/donnees/groupe/regimes/RegimeAlimentaire.java
@@ -0,0 +1,11 @@
+package fr.integration.donnees.groupe.regimes;
+
+public enum RegimeAlimentaire {
+	HYPOCALORIQUE,
+    HYPERCALORIQUE,
+    MEDITERRANEEN,
+    VEGETARIEN,
+    VEGAN,
+    SANS_GLUTEN,
+    SANS_LACTOSE
+}
